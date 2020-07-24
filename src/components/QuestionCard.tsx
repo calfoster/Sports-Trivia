bc5206c07f8f93fac6c72f9120dc9bfd33d24677
calfoster <gimpdevelopment@gmail.com>
2020-07-23 17:53:28 -0700

    Game logic

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNum} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
